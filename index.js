b28f919a10d27f8b887f45be780c9b91c79c1c75
devmihai <90305890+devmihai@users.noreply.github.com>
2021-09-17 22:19:21 +0300

    Updated portfolio
    Added some images and dark mode.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const navToggle = document.querySelector(".nav-toggle");
 const navLink = document.querySelectorAll(".nav__link");
+const icon = document.getElementById("moonicon")
+
 
 navToggle.addEventListener("click",() =>{
     document.body.classList.toggle("nav-open");
@@ -13,3 +15,27 @@ navLink.forEach(link => {
     })
 });
 
+if(localStorage.getItem("theme") === null) {
+localStorage.setItem("theme","light");
+}
+
+
+let localData = localStorage.getItem("theme");
+if(localData === "light"){
+    icon.src = "dark-mode.png";
+    document.body.classList.remove("dark-theme");
+} else if(localData === "dark") {
+    icon.src = "sun.png";
+    document.body.classList.add("dark-theme");
+}
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+if(document.body.classList.contains("dark-theme")){
+    icon.src ="sun.png";
+    localStorage.setItem("theme","dark");
+}else{
+    icon.src ="dark-mode.png";
+    localStorage.setItem("theme","light");
+}
+}
